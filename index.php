0c10a138311fde6f3aa1b178fe206a35a9dc42ff
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2022-02-17 18:03:22 -0300

    Adicionando modal e campos corretos dentro

--- index.php
+++ index.php
@@ -6,11 +6,54 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Bootstrap -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <title>PHP Crud com Modal</title>
+  <title>PHP Crud com ajax</title>
 </head>
 <body>
-  <div class="container">
-    <h1 class="text-center">Crud PHP com modal</h1>
+
+  
+
+  <!-- Modal -->
+  <div class="modal fade" id="completeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+        <div class="form-group">
+          <label for="completename">Nome</label>
+          <input type="text" class="form-control" id="completename" aria-describedby="emailHelp" placeholder="Digite seu nome">
+        </div>
+        <div class="form-group">
+          <label for="completeemail">E-mail</label>
+          <input type="email" class="form-control" id="completeemail" aria-describedby="emailHelp" placeholder="Digite seu e-mail">
+        </div>
+        <div class="form-group">
+          <label for="completephone">Telefone</label>
+          <input type="phone" class="form-control" id="completephone" aria-describedby="emailHelp" placeholder="Digite seu telefone">
+        </div>
+        <div class="form-group">
+          <label for="completeaddress">Endereço</label>
+          <input type="text" class="form-control" id="completeaddress" aria-describedby="emailHelp" placeholder="Digite seu endereço">
+        </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container my-3">
+    <h1 class="text-center">Tabela de usuários</h1>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#completeModal">
+      Adicionar usuário
+    </button>
   </div>
 
 <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script> -->
